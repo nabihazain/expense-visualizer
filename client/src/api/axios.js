b9c76f7668755ec13fb3b94de9dd680701afeabc
nabihazain <nabihazn2004@gmail.com>
2025-07-10 16:32:41 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend URL
+  baseURL: 'https://expense-visualizer-r84u.onrender.com/api', // Your backend URL
 });
 
 export default instance;
