a560a3756e90f4297d2870cd8381cebdb51e3f43
nabihazain <nabihazn2004@gmail.com>
2025-07-10 13:24:46 +0530

    Update axios.jss

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend URL
+  baseURL: process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : 'http://localhost:5000/api',
 });
 
 export default instance;
